885688ba47ad3de5debf8cdabd31f07246cfaf52
Vinicius Andrade <vinicius.andrade00@gmail.com>
2023-09-29 05:23:50 -0300

    adding BaseApiControllerWithResponsesHandler BaseApiControllerWithResponseHandler

---
+++ src/Package/Codescovery.Library.Api/Abstractions/BaseApiControllerWithResponseHandler.cs
@@ -0,0 +1,15 @@
+﻿using Codescovery.Library.Api.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace Codescovery.Library.Api.Abstractions;
+
+public class BaseApiControllerWithResponseHandler<TController> : BaseApiController<TController> where TController : BaseApiController<TController>
+{
+    public IResponseResultHandlers ResponseHandlers { get; }
+
+    public BaseApiControllerWithResponseHandler(IResponseResultHandlers responseHandlers, ILogger<TController> logger) : base(logger)
+    {
+        ResponseHandlers = responseHandlers;
+    }
+
+}
---
+++ src/Package/Codescovery.Library.Api/Abstractions/BaseApiControllerWithResponsesHandler.cs
@@ -0,0 +1,15 @@
+﻿using Codescovery.Library.Api.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace Codescovery.Library.Api.Abstractions;
+
+public abstract class BaseApiControllerWithResponsesHandler<TController>:BaseApiController<TController> where TController : BaseApiController<TController>
+{
+    public IResponseResultHandlers ResponseHandlers { get; }
+
+    public BaseApiControllerWithResponsesHandler(IResponseResultHandlers responseHandlers, ILogger<TController> logger):base(logger)
+    {
+        ResponseHandlers = responseHandlers;
+    }
+
+}
